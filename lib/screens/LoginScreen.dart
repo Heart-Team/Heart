35bc5858cc7379fe05fca1b44a49aadec2d6cc14
Sophie Baili <sophiehuangx@gmail.com>
2021-02-04 12:56:50 -0500

    fixed margin in LoginScreen

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -21,60 +21,67 @@ class LoginScreen extends StatelessWidget {
                 width: size.width * 0.55,
               ),
             ),
-            new Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              new Padding(padding: EdgeInsets.only(top: 260.0, left: 100)),
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 68, fontWeight: FontWeight.w600),
-              ),
-              new SizedBox(height: 20),
-              Text("Email", style: TextStyle(fontSize: 22)),
-              new SizedBox(height: 10),
-              new TextFormField(
-                  decoration: new InputDecoration(
-                labelText: "john@doe.com",
-                fillColor: Colors.white,
-                border: new OutlineInputBorder(
-                  borderSide: new BorderSide(),
-                ),
-                //fillColor: Colors.green
-              )),
-              new SizedBox(height: 10),
-              new Text("Password", style: TextStyle(fontSize: 22)),
-              new TextFormField(
-                  decoration: new InputDecoration(
-                labelText: "",
-                fillColor: Colors.white,
-                border: new OutlineInputBorder(
-                  // borderRadius: new BorderRadius.circular(35.0),
-                  borderSide: new BorderSide(),
-                ),
-              )),
-              new Text("Forgot password?",
-                  style: TextStyle(fontSize: 16, color: Colors.red)),
-              new SizedBox(height: 30),
-              new RaisedButton(
-                onPressed: () {},
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0.0),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[
-                        Color(0xFFFF6464),
-                        Color(0xFFFBBEBE),
-                        Color(0xFFFF6464),
-                      ],
-                    ),
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-                  child: const Text('Login',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
-                ),
-              ),
-            ])
+            Padding(
+                padding: EdgeInsets.only(top: 260.0, left: 30, right: 30),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 68, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 20),
+                      Text("Email", style: TextStyle(fontSize: 22)),
+                      SizedBox(height: 10),
+                      TextFormField(
+                          decoration: InputDecoration(
+                        labelText: "john@doe.com",
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      )),
+                      SizedBox(height: 10),
+                      Text("Password", style: TextStyle(fontSize: 22)),
+                      TextFormField(
+                          decoration: InputDecoration(
+                        labelText: "",
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                      )),
+                      Text("Forgot password?",
+                          style: TextStyle(fontSize: 16, color: Colors.red)),
+                      SizedBox(height: 30),
+                      Center(
+                        child: RaisedButton(
+                          onPressed: () {},
+                          textColor: Colors.white,
+                          padding: const EdgeInsets.all(0.0),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              gradient: LinearGradient(
+                                colors: <Color>[
+                                  Color(0xFFFF6464),
+                                  Color(0xFFFBBEBE),
+                                  Color(0xFFFF6464),
+                                ],
+                              ),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 90.0, vertical: 8),
+                            child: const Text('Login',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w600)),
+                          ),
+                        ),
+                      ),
+                    ]))
           ],
         ),
       ),
