7635f776e389dd00ba5de039a7a938f1e304d3d3
Sophie Baili <sophiehuangx@gmail.com>
2021-02-08 20:39:19 -0500

    added scrollView and all required charity info into CharityInfo widget

--- lib/widgets/charity_info/CharityInfo.dart
+++ lib/widgets/charity_info/CharityInfo.dart
@@ -19,9 +19,71 @@ class CharityInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[Text(this.charityName)],
+    Size size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.only(left: 10),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: size.height * 0.04,
+            ),
+            Text(this.charityName,
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.w600)),
+            Stack(children: <Widget>[
+              Text(this.location,
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400)),
+              Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.only(right: 26),
+                child: Image.asset(
+                  'assets/images/charity_info_screen/favorite_24px.png',
+                  height: size.height * 0.036,
+                ),
+              )
+            ]),
+            SizedBox(
+              height: 24,
+            ),
+            Text('Their Mission',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600)),
+            Text(this.mission,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.w400)),
+            SizedBox(
+              height: 24,
+            ),
+            Text('About',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600)),
+            Text(this.about,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400)),
+            SizedBox(
+              height: 24,
+            ),
+            Text('Charity Finances',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600)),
+            Text(this.finance,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.w400)),
+            SizedBox(
+              height: 24,
+            ),
+            Text('Learn More',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600)),
+            Text(this.website,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+          ]),
     );
   }
 }
