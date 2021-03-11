733f8aee08fd76c191e671d92c38d25324c15204
Sophie H <sophiehuangx@gmail.com>
2021-03-10 20:49:32 -0500

    search screen clear button and image set up

--- lib/screens/SearchScreen.dart
+++ lib/screens/SearchScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heart_app/Services/DataController.dart';
+import 'package:heart_app/widgets/App.dart';
 import '../theme.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -24,11 +25,14 @@ class _SearchState extends State<SearchScreen> {
         itemCount: snapshotData.documents.length,
         itemBuilder: (BuildContext context, int index){
           return ListTile(
-            // leading: CircleAvatar(
-            //   backgroundImage: NetworkImage(snapshotData.documents[index]['imageURL']),
-            // ),
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(snapshotData.documents[index]['imageURL']),
+            ),
             title: Text(snapshotData.documents[index].data['charityName'],
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),),
+            style: TextStyle(color:AppTheme().primaryColor, fontWeight: FontWeight.bold, fontSize: 20),),
+            subtitle: Text(snapshotData.documents[index].data['causeName']+ "\n" + snapshotData.documents[index].data['state']  ,
+                style: TextStyle(color: Colors.black, fontWeight: FontWeight.w200, fontSize: 16),),
           );
         },
       );
@@ -36,6 +40,15 @@ class _SearchState extends State<SearchScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppTheme().primaryColor,
+        child:Icon(Icons.clear, ),onPressed: (){
+          setState(() {
+            isExecuted = false;
+            searchController.text = "";
+          });
+      },
+      ),
       appBar:AppBar(
         backgroundColor: AppTheme().primaryColor,
         actions:[
@@ -56,6 +69,7 @@ class _SearchState extends State<SearchScreen> {
           )],
 
         title: TextField(
+
           style: TextStyle(color: Colors.white),
           decoration: InputDecoration(
             hintText: "Search charities here ...",
@@ -65,7 +79,14 @@ class _SearchState extends State<SearchScreen> {
         )
       ),
       body: isExecuted ? searchedData() : Container(
-        child: Center(child:Text('Empty search results')),
+        child: Center(child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Empty search results',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+            Image.asset('assets/images/empty_result.png'),
+           ])
+        )
       ),
     );
   }
